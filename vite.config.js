0f6baf5f2db02efb75add971857bcc45823f2061
Anuj2508 <anuj.y@ecodedash.com>
2025-06-27 17:33:00 +0530

    Changed the vite file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/langingPage/",
+  base: "/nfc-landingPage/",
   plugins: [react()],
 });
